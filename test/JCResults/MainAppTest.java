7a61981093b293df348d3a27fdf28922f719131d
Martyna Golas <D00193013@student.dkit.ie>
2020-10-27 18:31:32 +0000

    Added the test file

---
+++ test/JCResults/MainAppTest.java
@@ -0,0 +1,16 @@
+package JCResults;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainAppTest {
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void selectFiveGrades() {
+    }
+}
